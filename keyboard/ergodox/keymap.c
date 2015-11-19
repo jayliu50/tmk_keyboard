12620eb14dec244ddda2f64a8f8419012dc61cfa
Jay Liu <jayliu50@gmail.com>
2015-11-19 16:44:32 -0700

    Create test layout, along with Make target `test` for trying out new keyboards.

--- keyboard/ergodox/keymap.c
+++ keyboard/ergodox/keymap.c
@@ -87,6 +87,8 @@ along with this program.  If not, see <http://www.gnu.org/licenses/>.
 #include "keymap_cub.h"
 #elif defined(KEYMAP_GUNI)
 #include "keymap_guni.h"
+#elif defined(KEYMAP_TEST)
+#include "keymap_test.h"
 #else
 
 static const uint8_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
---
+++ keyboard/ergodox/keymap_test.h
@@ -0,0 +1,31 @@
+
+static const uint8_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
+    KEYMAP(  // layer 0 : default
+        // left hand
+        X,   X,   X,   X,   X,   X,   X,
+        X,   X,   X,   X,   X,   X,   X,
+        X,   X,   X,   X,   X,   X,
+        X,   X,   X,   X,   X,   X,   X,
+        X,   X,   X,   X,   X,
+                                      X,   X,
+                                           X,
+                                 X,   X,   X,
+        // right hand
+             X, X,   X,   X,   X,   X,   X,
+             X,X,   X,   X,   X,   X,   X,
+                  X,   X,   X,   X,   X,X,
+             X, X,   X,   X,X, X,X,
+                       X,X,X,  X,X,
+        X,X,
+        X,
+        X,X, X
+    )
+
+};
+
+/*
+ * Fn action definition
+ */
+static const uint16_t PROGMEM fn_actions[] = {
+
+};
